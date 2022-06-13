14b790c0c5874ce6f59eb3884b62bb8dd9765c6b
Aleksandar Osenov <aosenov@gmail.com>
2022-06-13 19:27:36 +0300

    Fix urls

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 
 export var apiBaseUrl = process.env.NODE_ENV === 'production' ?
-    'https://mern-photo-sharing-app.herokuapp.com/' :
-    'http://localhost:5000/';
+    'https://mern-photo-sharing-app.herokuapp.com' :
+    'http://localhost:5000';
 
 
 ReactDOM.render(
